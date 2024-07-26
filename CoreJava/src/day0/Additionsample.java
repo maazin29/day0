3ad57090b638cbfe20399e9756b3dab85c766833
almaa <almaa@maaz>
2024-07-26 15:56:44 +0530

    this is the first file of java

---
+++ CoreJava/src/day0/Additionsample.java
@@ -0,0 +1,23 @@
+package day0;
+
+import java.util.Scanner;
+
+public class Additionsample {
+
+	public static void main(String[] args) {  
+			int a,b,c;
+		         Scanner sc=new Scanner(System.in);
+		         a=sc.nextInt();
+		         b=sc.nextInt();
+		         sc.nextLine();
+		         String name;
+		         name=sc.nextLine();
+		         c=a+b;
+		          System.out.println("addtion "+c);
+		          System.out.println("name of trainer"); 
+		}
+		
+		
+	}
+
+
